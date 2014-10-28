f92c87d4cdd53eeec09054f496038a3bdfc4cef9
reidliujun <liuj2@niksula.hut.fi>
2014-10-28 19:41:45 +0200

    modified the application

--- crowdsensing/test_app/src/com/example/test/MainActivity.java
+++ crowdsensing/test_app/src/com/example/test/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.ContextWrapper;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,94 +33,98 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final String path = this.getFilesDir().getAbsolutePath();
-//        Log.i("path:", path);//data/data/com.example.test/files
         final TextView txt = (TextView) findViewById(R.id.textview);
         final Button button = (Button) findViewById(R.id.scan);
-        final WifiManager manager;
-        manager = (WifiManager) getSystemService(WIFI_SERVICE);
-        
+        final Context mContext = this;
+        final int times = 15;
+        final int interval = 5;
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            	String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()); 
-            	File file_json = new File(path + "/result_"+timeStamp+".json");
-            	File file_csv = new File(path + "/result_"+timeStamp+".csv");
-            	String APinfoString="";
-            	int i=1;
-            	JSONObject multi_location = new JSONObject();
-                try {
-                    while (true) {
-                    	List<ScanResult> results = manager.getScanResults();
-                    JSONObject available = new JSONObject();
-                      try {
-                    	  int j=1;
-              			for (ScanResult scanResult : results) {
-              				
-              				JSONObject accessPoint = new JSONObject();
-              				accessPoint.put("BSSID", scanResult.BSSID);
-              				accessPoint.put("SSID", scanResult.SSID);
-              				accessPoint.put("frequency", scanResult.frequency);
-              				accessPoint.put("level", scanResult.level);
-              				accessPoint.put("capabilities", scanResult.capabilities);
-//              				available.put(accessPoint);
-              				available.put("ap"+j, accessPoint);
-              				APinfoString+="BSSID,"+scanResult.BSSID+",SSID,"+scanResult.SSID+",level,"+Integer.toString(scanResult.level)+"\n";
-              				j++;
-              			}
-	              		} catch (JSONException e) {
-	              			e.printStackTrace();
-	              		}
-//                      multi_location.put("id",);
-                      	try {
-                      		manager.startScan();
-                      		multi_location.put("id"+i, available);
-                      		Log.i("wifi available:", i+"th data set");
-                      		Log.i("wifi available:", available.toString());
-                      		Log.i("wifi multi:", multi_location.toString());
-						} catch (JSONException e2) {
-							// TODO Auto-generated catch block
-							e2.printStackTrace();
-						}
-                      	
-                        Thread.sleep(15 * 1000);
-                        i=i+1;
-                        if (i>4){
-                        	try {
-                    			FileOutputStream stream_json = new FileOutputStream(file_json);
-                    			stream_json.write(multi_location.toString().getBytes());
-                    			FileOutputStream stream_csv = new FileOutputStream(file_csv);
-                    			stream_csv.write(APinfoString.getBytes());
-                    		} catch (FileNotFoundException e1) {
-                    			// TODO Auto-generated catch block
-                    			e1.printStackTrace();
-                    		} catch (IOException e) {
-                    			// TODO Auto-generated catch block
-                    			e.printStackTrace();
-                    		}
-                        	txt.setText("Scan finished!");
-                        	break;
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            	new Thread(new Runnable() { 
+					public void run(){
+						startScan(mContext, times, interval);
+	                    }
+	            }).start();
             }
         });
-        
-
     }
 
-    // convert from dBm to dB
-    public int getPowerPercentage(int power) {
-        int i = 0;
-        int MIN_DBM = -100;
-        if (power <= MIN_DBM) {
-            i = 0;
-        } else {
-            i = 100 + power;
+    // scan wifi results write to /sdcard/tmp and vibrate after finished
+    public void startScan(Context mContext, int times, int interval){
+        String path = "/sdcard/tmp";
+        Log.i("path:", path);//data/data/com.example.test/files
+        WifiManager manager;
+        manager = (WifiManager) getSystemService(WIFI_SERVICE);
+    	String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()); 
+    	File file_json = new File(path + "/result_"+timeStamp+".json");
+    	File file_csv = new File(path + "/result_"+timeStamp+".csv");
+    	String APinfoString="";
+    	int i=1;
+    	JSONObject multi_location = new JSONObject();
+        try {
+            while (true) {
+            	List<ScanResult> results = manager.getScanResults();
+            JSONObject available = new JSONObject();
+              try {
+            	  int j=1;
+      			for (ScanResult scanResult : results) {
+      				
+      				JSONObject accessPoint = new JSONObject();
+      				accessPoint.put("BSSID", scanResult.BSSID);
+      				accessPoint.put("SSID", scanResult.SSID);
+      				accessPoint.put("frequency", scanResult.frequency);
+      				accessPoint.put("level", scanResult.level);
+      				accessPoint.put("capabilities", scanResult.capabilities);
+//      				available.put(accessPoint);
+      				available.put("ap"+j, accessPoint);
+      				APinfoString+="BSSID,"+scanResult.BSSID+",SSID,"+scanResult.SSID+",level,"+Integer.toString(scanResult.level)+"\n";
+      				j++;
+      			}
+          		} catch (JSONException e) {
+          			e.printStackTrace();
+          		}
+//              multi_location.put("id",);
+              	try {
+              		manager.startScan();
+              		multi_location.put("measurement "+i, available);
+              		Log.i("wifi available:", i+"th data set");
+              		Log.i("wifi available:", available.toString());
+              		Log.i("wifi multi:", multi_location.toString());
+				} catch (JSONException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+                Thread.sleep(interval * 1000);
+                i=i+1;
+                if (i>times){
+                	try {
+            			FileOutputStream stream_json = new FileOutputStream(file_json);
+            			stream_json.write(multi_location.toString().getBytes());
+            			FileOutputStream stream_csv = new FileOutputStream(file_csv);
+            			stream_csv.write(APinfoString.getBytes());
+            			
+            		} catch (FileNotFoundException e1) {
+            			// TODO Auto-generated catch block
+            			e1.printStackTrace();
+            		} catch (IOException e) {
+            			// TODO Auto-generated catch block
+            			e.printStackTrace();
+            		}
+                	vibrate(mContext, 1);
+                	break;
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        return i;
     }
+    
+    public void vibrate(Context mContext, int seconds){
+    	Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+   	 // Vibrate for 500 milliseconds
+    	v.vibrate(seconds*1000);
+    }
+    
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
